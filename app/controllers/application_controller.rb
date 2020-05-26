ad4f1d6b23581aeea5bc700b87c1f2c506e25c45
StlMaris123 <stellamarisnjage@gmail.com>
2020-05-26 15:36:59 +0200

    redis sessions and bootsrap setup

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,16 @@
 class ApplicationController < ActionController::Base
+    protect_from_forgery prepend: true
+    skip_before_action :verify_authenticity_token, if: :json_request?
+    
+    before_action :find_current_user
+    
+    private
+  
+    def find_current_user
+       @user = User.find_by_session_id(session&.id)
+    end
+  
+    def json_request?
+      request.format.json?
+    end 
 end
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,11 @@
+Rails.application.config.session_store :redis_session_store, {
+    key: '_be_free_session',
+    serializer: :json,
+    domain: :all,
+    tld_length: 2,
+    redis: {
+      expire_after: 1.week,
+      key_prefix: 'be_free:session',
+      url: ENV['REDIS_URL']
+    }
+ }
